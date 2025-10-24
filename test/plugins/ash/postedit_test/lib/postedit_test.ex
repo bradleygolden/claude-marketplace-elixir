c98590bef408bffa35361161a358947a8f96aab7
Bradley Golden <golden.bradley@gmail.com>
2025-10-24 01:08:08 -0500

    🔥 initial commit 🔥

---
+++ test/plugins/ash/postedit_test/lib/postedit_test.ex
@@ -0,0 +1,18 @@
+defmodule PosteditTest do
+  @moduledoc """
+  Documentation for `PosteditTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PosteditTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
